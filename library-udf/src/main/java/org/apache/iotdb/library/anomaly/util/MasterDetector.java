cc7760787772b59b7ea86e4c12a9b97b20277a73
VGalaxies <vgalaxies@apache.org>
2025-05-28 00:18:39 +0800

    compile

--- library-udf/src/main/java/org/apache/iotdb/library/anomaly/util/MasterDetector.java
+++ library-udf/src/main/java/org/apache/iotdb/library/anomaly/util/MasterDetector.java
@@ -1,8 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iotdb.library.anomaly.util;
 
 import org.apache.iotdb.library.util.Util;
 import org.apache.iotdb.udf.api.access.Row;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
@@ -34,7 +54,7 @@ public class MasterDetector {
     this.beta = beta;
   }
 
-  public boolean isNullRow(Row row) {
+  public boolean isNullRow(Row row) throws IOException {
     boolean flag = true;
     for (int i = 0; i < row.size(); i++) {
       if (!row.isNull(i)) {
